272ff0a3cc00d9cce15a44cd0efcc918a16faf52
aliasamot <alialorens09@gmail.com>
2024-09-26 17:02:54 +0800

    Update Home.py

--- project/Home.py
+++ project/Home.py
@@ -8,7 +8,7 @@ st.markdown(
 
    <style>
     .main {
-        background-color: #D2E0FB
+        background-color: #FFF4EA
     }
     </style>
 
@@ -16,25 +16,26 @@ st.markdown(
         @import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap');
     
     /* Button styles */
-
     .stButton > button {
-        background-color: #7695FF;
-        border-radius: 12px;
-        padding: 10px 24px;
-        color: white;
-        font-size: 16px;
-        font-family: 'Quicksand', sans-serif;
-        font-weight: bold;
+    background-color: #982B1C; 
+    border-radius: 12px;
+    padding: 10px 24px;
+    color: white; /* Text color */
+    font-size: 16px;
+    font-family: 'Quicksand', sans-serif;
+    font-weight: bold;
+    border: none; 
     }
 
     /* Hover effect for buttons */
     .stButton > button:hover {
-        background-color: #125B9A;
-        color: white;
+    background-color: #800000; 
+    color: white; 
     }
+
     /* Header style */
     header {
-        background-color: #227B94;
+        background-color: #800000;
         padding: 15px;
         display: flex;
         justify-content: center;
@@ -57,7 +58,7 @@ st.markdown(
     h3 {
         font-family: 'Libre Baskerville', sans-serif;
         font-size: 2em;
-        color: #227B94;
+        color: #800000;
         justify-content: center;
     }
 
@@ -65,59 +66,28 @@ st.markdown(
         font-family: "Quicksand", system-ui;
         font-size: 1em;
         line-height: 1.5em;
-        color: #333333;
+        color: black;
     }
 
     /* Expander styling */
         .expanderHeader {
             font-family: "Quicksand", sans-serif;
             font-size: 1em;
-            color: #333333;
+            color: black;
         }
      .expanderContent {
             font-family: "Quicksand", sans-serif;
             font-size: 1em;
-            color: #333333;
+            color: black;
             margin: 10px 0;
         }
-    /* List styles */
-        ul {
-            list-style-type: disc;
-            margin-left: 20px;
-        }
-
-        .heat-index {
-            font-style: normal;
-            color: #333333;
-        }
-
-        .water-pollution {
-            font-style: normal;
-            color: #333333;
-        }
-
-        .population-growth {
-            font-style: normal;
-            color: #333333;
-        }
-
-        .agricultural-activities {
-            font-style: normal;
-            color: #333333;
-        }
-
-        .climate-change {
-            font-style: normal;
-            color: #333333;
-        }
-
 
     /* Footer styles */
     footer {
         font-family: "Quicksand", system-ui;
         text-align: center;
         padding: 15px;
-        background-color: #16325B;
+        background-color: #800000;
         color: white;
         margin-top: 20px;
         font-size: 1em;
@@ -139,7 +109,7 @@ logo_base64 = image_to_base64("logo.png")
 st.markdown(f"""
 <header style="display: flex; align-items: center;">
     <img src="data:image/png;base64,{logo_base64}" alt="Logo" style="max-height: 60px; margin-right: 20px;">
-    <h1>Water Scarcity Forecast</h1>
+    <h1>Dengue Outbreak Forecast</h1>
 </header>
 """, unsafe_allow_html=True)
 
@@ -156,25 +126,28 @@ with tab1:
 
     with col2:
             st.markdown("""
-            ### What is Water Scarcity?
-            Water scarcity is defined as the state of imbalance between water demand and supply, where water availability is compromised. This has been a pressing issue as faced by many countries due to several factors but water scarcity factors remain under explored. Additionally, water scarcity is the lack of water resources to meet the standard water demand. There are two types of water scarcity: physical, and economic water scarcity. Physical water scarcity is where there is not enough water to meet all demands.
+            ### What is Dengue?
+            Dengue, also known as break-bone fever, is a viral infection that spreads and is transmitted through mosquito bites to people. It commonly occurs in tropical and subtropical climates. In addition, it was known to have no specific treatment, therefore, early detection and access to proper medical care can greatly lower fatality rates of dengue, (World Health Organization, 2024).
             """)
-
+    with st.expander("Learn More"):
+                            st.markdown("""
+            Moreover, dengue fever is considered to be one of the top ten threats to global health (Lowe et al., 2021). Thus, it was reported that there are millions of cases of dengue infection occurring worldwide each year (Mayo Clinic staff, 2024). Therefore, immediate action for prevention and detection must be established.
+            """)
     # Global and local water scarcity details
     with st.container():
         col1, col2 = st.columns([1.3, 1])
 
         with col1:
                 st.markdown("""
-            ### Water Scarcity in the World
-            Water scarcity is due to the lack of sufficient water and lack of access to safe water supplies. The phenomena has been affecting the lives of many population across the globe. Moreover, according to Kuzma (2023), this problem results in water stress, a condition that many countries experience where there is an imbalance between the supply and demand of water."
+            ### Dengue in the World
+            Dengue fever is an arboviral virus that is considered one of the top ten risks to global health (Lowe et al., 2021). Additionally, according to the World Health Organization (2024) half of the world's population is now at risk of dengue, with an estimated 100–400 million infections occurring each year.
                             """)
             
         with col2:
             st.image("world.jpg", caption="© Unicef", width=300)
         with st.expander("Learn More"):
                             st.markdown("""
-            Furthermore, access to water remains a development issue across the continent, specifically in Africa, with one in three Africans facing water scarcity. However, despite the action against the water scarcity, the phenomenon remains a massive issue across the world. One of the factors of water scarcity is the shortage in the supply of consumable water brought by several factors. Yet, the water scarcity still prevails due to the climate change, the environmentally harmful and water-intensive activities of humans (Ingrao et al., 2023).
+            In addition, according to the European Centre for Disease Prevention and Control (2024), there has been a substantial increase in the number of imported cases of dengue to the EU/EEA since the beginning of 2024, resulting in over 12 million dengue cases, and over 8000 dengue-related deaths have been reported from 86 countries. However, there are many endemic countries that have no strong detection and reporting mechanisms, resulting in underestimated detection of dengue globally (World Health Organization, 2024)
             """)
             
     # Philippine water scarcity details
@@ -186,64 +159,44 @@ with tab1:
 
         with col2:
             st.markdown("""
-            ### Water Scarcity in the Philippines
-            The Philippines is no excuse from the pervasiveness of water scarcity as statistics recorded in year 2020 implied that 57 million out of 109 million people (52%) lack access to consumable and safely managed water and 39% of those suffers from the absence of proper water sanitation systems according to Water.org in 2020. 
+            ### Dengue in the Philippines
+            Dengue still remains as a serious health concern in the Philippines. It was considered to be endemic and during 2008–2012, the country's Department of Health reported an annual average of 117,065 dengue cases which placed the country fourth in dengue burden in southeast Asia (Edillo et al., 2024).
             """)
        
         with st.expander("Learn More"):
             st.markdown("""
-        Metro Manila is one specific region where water scarcity is felt with 13 million of its population experiencing water shortages stated by the National Public Radio in 2023. In more recent events, according to the report of the Philippine Atmospheric, Geophysical and Astronomical Services Administration (PAGASA), CALABARZON region has been facing droughts since 2024 started. Cavite was hit with drought, Rizal with dry spell, and Laguna and Batangas with dry conditions base on the study of Odong in 2024. It was also reported by the GMA news that Barangay Bucandala 3, an area in Imus Cavite, experienced an unsteady water supply for 6 months in 2022. This indicates the urgency of recognizing such nationwide phenomenon.
+        While, from January to December of year 2023 have been reported 657 deaths (case fatality rate, 0.34%). Thus, there is still a need for continued dengue prevention, as the programs for dengue prevention established by the stakeholders were not effective in decreasing the dengue cases. (Cordero, D., 2024). In addition, according to the Department of Health (2024) there is an increase of dengue cases in the Philippines for about 15 %, including 77,867 dengue cases have been logged with 205 deaths as the month of June.
         """)
 
-    st.markdown("""
-    ### Factors affecting water scarcity
-    The notable factors which contribute to water scarcity are heat index, water pollution, population growth, agricultural activities, and climate change. 
-    <ul>
-    <li class="heat-index">
-        <strong>Heat index</strong> - as it rises, the probability of drought occuring also increases.  The demands also increases especially in El Niño season. Thus lessened the available water needed.
-    </li>
-    <li class="water-pollution">
-        <strong>Water pollution</strong> -  the main cause of it was human activities such as throwing trashes everywhere. This contaminates the bodies of water or available freshwater. As a result, the water cannot be safely consumed or used, descreasing the available water for us to consume.
-    </li>
-    <li class="population-growth">
-        <strong>Population growth</strong> - the increase of population also increases the water demand needed to supply each sectors. This contributes to water stress.
-    </li>
-    <li class="agricultural-activities">
-        <strong>Agricultural activities</strong> - contributes to the prevalence of water scarcity. In was recorded that 60% of the used fresh water by agricultural activities is wasted due to leaky irrigation systems, inefficient application methods, and the cultivation of crops thirsty for the environment they were grown in. According to the data of World Wide Fund (WWF), year 2024.
-    </li>
-    <li class="climate-change">
-        <strong>Climate change</strong>  that which exacerbates this phenomena. Human activities that produces more carbon dioxide  continuously affect weather patterns as these compounds intoxicates the atmosphere, as result, drought seasons will be more occurring and glaciers will vanish, limiting the supply of water, according to World Wide Fund (WWF) also as of 2024.
-    </li>
-    </ul>
-    """, unsafe_allow_html=True)
 
 with tab2:
 
 # Information about the factors
-    st.markdown("""        
-    ### Heat Index
-
-    Also known as the apparent temperature, it indicates the level of discomfort that the person is expected to experience due to the combined effects of air temperature and humidity. The heat index is directly proportional to the humidity. Therefore, as humidity increases, the heat index also increases, and when the humidity decreases, the heat index also decreases. 
-    """)
 
     st.markdown("""    
-    ### Rainfall
+    ### Waste
 
     The total amount of rain falling in a specific area within a particular time frame.
     """)
 
     st.markdown("""    
-    ### Total Water Withdrawal
+    ### Flood
 
-    Total water withdrawal is the yearly freshwater withdrawal in which the freshwater is taken from ground or surface water sources and can be permanent or temporary. Then, transferred to a place of use.
+    A flood is defined as an overflow of water that submerges land that is usually dry, most of the time as a result of excessive rainfall or storm surges. Floods in the Philippines are some of the common natural occurrences that pose significant risk to people and their physical structures within the region. For instance, as of 2024, areas in the Philippines especially Mindanao are currently affected by significant experiencing floods and landslides due continuous rainfall which has affected 882,861 people of whom 612,234 are displaced. Some of the factors that causes flooding includes deforestation, ineffective waste disposal methods, poor drainage channel and structure designs, Inadequate maintenance of drainage facilities, blockage by debris brought by flood waters and the effects of climate change, which increase rainfall intensity and frequency. Floods are recorded through various methods, including satellite imagery, river gauge or steam gauge readings, and meteorological data collected from weather stations. Such tools are effective towards certain agencies like PAGASA, in determining the water level and forecasting possible flood occurrences. As a result, it has been clear that flood is indeed measurable, as their impact can be quantified in terms of water level or depth, volume of area affected, and duration, providing essential information for an effective disaster response.
     """)
 
+    st.markdown("""        
+    ### Rainfall
+
+    The total amount of rain falling in a specific area within a particular time frame.
+    """)
+    
     st.markdown("""    
     ### Regression Analysis
-
+""")
+    
     if st.button("Regression Analysis"):
         st.switch_page("pages/Regression Analysis.py")
-    """)
 
 # Custom footer
 st.markdown("""
