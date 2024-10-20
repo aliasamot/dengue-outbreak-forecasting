79b567c81ce2749dc92dd000740cd747266ca427
aliasamot <alialorens09@gmail.com>
2024-10-20 18:18:28 +0800

    Update Amount of Waste.py

--- project/pages/Amount of Waste.py
+++ project/pages/Amount of Waste.py
@@ -45,7 +45,7 @@ st.markdown(
 
     header h1 {
         font-family: 'Libre Baskerville', sans-serif;
-        font-size: 1.7em;
+        font-size: 1.6em;
         color: white;
         text-shadow: 1px 1px 2px #000000;
         margin: 0;
